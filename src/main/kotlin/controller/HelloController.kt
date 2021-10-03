13eac27256db861094dd5db253f81ba49ec2b60d
AndoniSalcedo <57041252+AndoniSalcedo@users.noreply.github.com>
2021-10-03 19:50:54 +0200

    new get route added (#18)
    * Test MVC and Unit for new route
    * bin removed and gitignore update to avoid this file
    * Fix .gitignore
    * Don't mix unit and mvc tests

--- src/main/kotlin/controller/HelloController.kt
+++ src/main/kotlin/controller/HelloController.kt
@@ -2,7 +2,7 @@ package es.unizar.webeng.hello.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.*
 
 /**
  * **Note**
@@ -47,6 +47,21 @@ class HelloController {
         // This is used to associate the variable "message" of the template welcome with a value.
         model["message"] = message
         return "welcome"
+    }
+
+   /**
+    * This function acts as the handler of the HelloController.
+    * shows a template saying hello to the parameter pased by url
+    * 
+    * **Note**
+    * @param name parameter passed by url
+    * @param model collection with the data used to update the view (template)
+    * @return the template with the updated information
+    */
+    @GetMapping("/name/{name}")
+    fun new(@PathVariable name: String, model: MutableMap<String, Any>): String {
+        model["message"] = "Hello " + name 
+        return "new"
     }
 
     /**
